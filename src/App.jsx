aa751d0e434e02a6dbea6103984cbdc5e3ea7fcb
Alex Alba <78807137+AlexAlcar@users.noreply.github.com>
2024-03-03 19:18:11 +0100

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
       <Routes>
       <Route path="/" element={<ChooseSources />} />
         <Route path="/news" element={<ViewNews />} />
-        <Route path="/sources" element={<ChooseSources />} />
       </Routes>
     </Router>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sources', // Establece la ruta base predeterminada
-  plugins: [react()]
-});
+  plugins: [react()],
+})
